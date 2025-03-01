8f2da8b937aa6d076cd7f699238a2a00ad393f38
Benjamin Chang <bchang@illinois.edu>
2025-03-01 17:08:43 -0600

    change info box text size

--- src/lib/components/InfoBox.tsx
+++ src/lib/components/InfoBox.tsx
@@ -12,8 +12,8 @@ export default function InfoBox({ title, content, link }: InfoBox) {
       href={link}
       className="flex flex-col py-8 px-4 rounded-md border-gray-200 border-[1px] w-96 h-48 gap-4 hover:bg-gray-200"
     >
-      <div className="font-bold text-black">{title}</div>
-      <div>{content}</div>
+      <div className="font-bold text-black text-2xl">{title}</div>
+      <div className="text-lg">{content}</div>
     </Link>
   );
 }
