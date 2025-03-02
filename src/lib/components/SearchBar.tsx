9effedfaa1707152e99c6a6e5c8ee464a17260c2
Benjamin Chang <bchang@illinois.edu>
2025-03-02 01:36:23 -0600

    add search functionality

--- src/lib/components/SearchBar.tsx
+++ src/lib/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as emoji from "node-emoji";
 import Link from "next/link";
 import { getDateString, Issue } from "../utils/utils";
@@ -40,6 +40,11 @@ export default function SearchBar() {
     week: [],
     month: [],
   });
+  const [dayIssues, setDayIssues] = useState<Issue[]>(issues.day);
+  const [weekIssues, setWeekIssues] = useState<Issue[]>(issues.week);
+  const [monthIssues, setMonthIssues] = useState<Issue[]>(issues.month);
+
+  const searchBarRef = useRef<HTMLInputElement>(null);
   const getIssues = async () => {
     const res = await fetch("/api/getissues", {
       method: "GET",
@@ -49,10 +54,45 @@ export default function SearchBar() {
     });
     const data = await res.json();
     setIssues(data["issues"] as Issues);
+    setDayIssues(data["issues"]["day"] as Issue[]);
+    setMonthIssues(data["issues"]["month"] as Issue[]);
+    setWeekIssues(data["issues"]["week"] as Issue[]);
   };
   useEffect(() => {
     getIssues();
   }, []);
+
+  const updateSearch = () => {
+    if (searchBarRef.current !== null) {
+      setDayIssues(
+        issues.day.filter((issue) => {
+          return searchBarRef.current?.value
+            ? issue.title
+                .toLowerCase()
+                .includes(searchBarRef.current?.value.toLowerCase()!)
+            : true;
+        }),
+      );
+      setWeekIssues(
+        issues.week.filter((issue) => {
+          return searchBarRef.current?.value
+            ? issue.title
+                .toLowerCase()
+                .includes(searchBarRef.current?.value.toLowerCase()!)
+            : true;
+        }),
+      );
+      setMonthIssues(
+        issues.month.filter((issue) => {
+          return searchBarRef.current?.value
+            ? issue.title
+                .toLowerCase()
+                .includes(searchBarRef.current?.value.toLowerCase()!)
+            : true;
+        }),
+      );
+    }
+  };
   const emojis = [
     "house",
     "rocket",
@@ -81,50 +121,55 @@ export default function SearchBar() {
             <path d="M80-200v-80h400v80H80Zm0-200v-80h200v80H80Zm0-200v-80h200v80H80Zm744 400L670-354q-24 17-52.5 25.5T560-320q-83 0-141.5-58.5T360-520q0-83 58.5-141.5T560-720q83 0 141.5 58.5T760-520q0 29-8.5 57.5T726-410l154 154-56 56ZM560-400q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Z" />
           </svg>
           <input
+            ref={searchBarRef}
             className="w-full outline-none"
             placeholder="Search for an issue..."
+            onChange={updateSearch}
           ></input>
         </div>
-        {issues.day.length + issues.month.length + issues.week.length > 0 && (
-          <div className="group-focus:flex bg-white group-focus-within:flex flex-col gap-4 absolute top-0 mt-12 border-b-[1px] border-r-[1px] rounded-br-sm rounded-bl-sm border-l-[1px] border-gray-200 h-fit max-h-96 w-full p-4 hidden overflow-scroll">
-            {issues.day.length > 0 && (
-              <div className="flex flex-col gap-2">
-                <div className="text-gray-400 font-semibold">Today</div>
-                <IssueDisplay
-                  issues={issues.day}
-                  emojis={emojis.slice(0, issues.day.length)}
-                  showDate={false}
-                ></IssueDisplay>
-              </div>
-            )}
+        {dayIssues !== undefined &&
+          weekIssues !== undefined &&
+          monthIssues !== undefined &&
+          dayIssues.length + monthIssues.length + weekIssues.length > 0 && (
+            <div className="group-focus:flex bg-white group-focus-within:flex flex-col gap-4 absolute top-0 mt-12 border-b-[1px] border-r-[1px] rounded-br-sm rounded-bl-sm border-l-[1px] border-gray-200 h-fit max-h-96 w-full p-4 hidden overflow-scroll">
+              {dayIssues.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <div className="text-gray-400 font-semibold">Today</div>
+                  <IssueDisplay
+                    issues={dayIssues}
+                    emojis={emojis.slice(0, dayIssues.length)}
+                    showDate={false}
+                  ></IssueDisplay>
+                </div>
+              )}
 
-            {issues.week.length > 0 && (
-              <div className="flex flex-col gap-2">
-                <div className="text-gray-400 font-semibold">Past Week</div>
-                <IssueDisplay
-                  issues={issues.week}
-                  emojis={emojis.slice(
-                    issues.day.length,
-                    issues.day.length + issues.week.length,
-                  )}
-                ></IssueDisplay>
-              </div>
-            )}
+              {weekIssues.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <div className="text-gray-400 font-semibold">Past Week</div>
+                  <IssueDisplay
+                    issues={issues.week}
+                    emojis={emojis.slice(
+                      dayIssues.length,
+                      dayIssues.length + weekIssues.length,
+                    )}
+                  ></IssueDisplay>
+                </div>
+              )}
 
-            {issues.month.length > 0 && (
-              <div className="flex flex-col gap-2">
-                <div className="text-gray-400 font-semibold">Past Month</div>
-                <IssueDisplay
-                  issues={issues.month}
-                  emojis={emojis.slice(
-                    issues.day.length + issues.week.length,
-                    emojis.length,
-                  )}
-                ></IssueDisplay>
-              </div>
-            )}
-          </div>
-        )}
+              {monthIssues.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <div className="text-gray-400 font-semibold">Past Month</div>
+                  <IssueDisplay
+                    issues={monthIssues}
+                    emojis={emojis.slice(
+                      dayIssues.length + weekIssues.length,
+                      emojis.length,
+                    )}
+                  ></IssueDisplay>
+                </div>
+              )}
+            </div>
+          )}
       </div>
     </div>
   );
